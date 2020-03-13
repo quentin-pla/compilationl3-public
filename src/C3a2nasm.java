a1c7f2c56f2bf47f7ddfd377a6bde047c0858edb
Quentin Pla <quentin-pla@hotmail.fr>
2020-03-13 17:31:37 +0100

    Mise à jour des fichiers + création classe c3a2nasm

---
+++ src/C3a2nasm.java
@@ -0,0 +1,122 @@
+import c3a.*;
+import nasm.Nasm;
+import nasm.NasmOperand;
+import ts.Ts;
+import ts.TsItemFct;
+
+public class C3a2nasm implements C3aVisitor<NasmOperand> {
+    private C3a c3a;
+    private Nasm nasm;
+    private Ts tableGlobale;
+    private TsItemFct currentFct;
+
+    @Override
+    public NasmOperand visit(C3aInstAdd inst) {
+        return null;
+    }
+
+    @Override
+    public NasmOperand visit(C3aInstCall inst) {
+        return null;
+    }
+
+    @Override
+    public NasmOperand visit(C3aInstFBegin inst) {
+        return null;
+    }
+
+    @Override
+    public NasmOperand visit(C3aInst inst) {
+        return null;
+    }
+
+    @Override
+    public NasmOperand visit(C3aInstJumpIfLess inst) {
+        return null;
+    }
+
+    @Override
+    public NasmOperand visit(C3aInstMult inst) {
+        return null;
+    }
+
+    @Override
+    public NasmOperand visit(C3aInstRead inst) {
+        return null;
+    }
+
+    @Override
+    public NasmOperand visit(C3aInstSub inst) {
+        return null;
+    }
+
+    @Override
+    public NasmOperand visit(C3aInstAffect inst) {
+        return null;
+    }
+
+    @Override
+    public NasmOperand visit(C3aInstDiv inst) {
+        return null;
+    }
+
+    @Override
+    public NasmOperand visit(C3aInstFEnd inst) {
+        return null;
+    }
+
+    @Override
+    public NasmOperand visit(C3aInstJumpIfEqual inst) {
+        return null;
+    }
+
+    @Override
+    public NasmOperand visit(C3aInstJumpIfNotEqual inst) {
+        return null;
+    }
+
+    @Override
+    public NasmOperand visit(C3aInstJump inst) {
+        return null;
+    }
+
+    @Override
+    public NasmOperand visit(C3aInstParam inst) {
+        return null;
+    }
+
+    @Override
+    public NasmOperand visit(C3aInstReturn inst) {
+        return null;
+    }
+
+    @Override
+    public NasmOperand visit(C3aInstWrite inst) {
+        return null;
+    }
+
+    @Override
+    public NasmOperand visit(C3aConstant oper) {
+        return null;
+    }
+
+    @Override
+    public NasmOperand visit(C3aLabel oper) {
+        return null;
+    }
+
+    @Override
+    public NasmOperand visit(C3aTemp oper) {
+        return null;
+    }
+
+    @Override
+    public NasmOperand visit(C3aVar oper) {
+        return null;
+    }
+
+    @Override
+    public NasmOperand visit(C3aFunction oper) {
+        return null;
+    }
+}
